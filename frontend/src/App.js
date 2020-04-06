98fa154b7dd8386290bc5aaead07578a4f6ccfbb
sagardspeed2 <sagardspeed2@gmail.com>
2020-04-06 20:38:55 +0530

    API change - react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   }
   refreshList = () => {
     axios
-      .get("http://localhost:8000/api/todos/")
+      .get("https://django-react-todo.herokuapp.com/api/todos/")
       .then(res => this.setState({ todoList: res.data }))
       .catch(err => console.log(err));
   };
@@ -91,17 +91,17 @@ class App extends Component {
     this.toggle();
     if (item.id) {
       axios
-        .put(`http://localhost:8000/api/todos/${item.id}/`, item)
+        .put(`https://django-react-todo.herokuapp.com/api/todos/${item.id}/`, item)
         .then(res => this.refreshList());
       return;
     }
     axios
-      .post("http://localhost:8000/api/todos/", item)
+      .post("https://django-react-todo.herokuapp.com/api/todos/", item)
       .then(res => this.refreshList());
   };
   handleDelete = item => {
     axios
-      .delete(`http://localhost:8000/api/todos/${item.id}`)
+      .delete(`https://django-react-todo.herokuapp.com/api/todos/${item.id}`)
       .then(res => this.refreshList());
   };
   createItem = () => {
